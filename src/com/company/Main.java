aa1cb4751b911dc7665408702674e5d0608486a7
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-05-05 16:40:47 +0900

    練習問題1の呼び出しを変更

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,37 +6,7 @@ public class Main {
         HelloWorld hw = new HelloWorld();
         hw.main();
 
-        Substitution_1_2 sub_1_2 = new Substitution_1_2();
-        sub_1_2.main();
-
-        Substitution_1_3 sub_1_3 = new Substitution_1_3();
-        sub_1_3.main();
-
-        Substitution_1_4 sub_1_4 = new Substitution_1_4();
-        sub_1_4.main();
-
-        Substitution_1_5 sub_1_5 = new Substitution_1_5();
-        sub_1_5.main();
-
-        Substitution_1_6 sub_1_6 = new Substitution_1_6();
-        sub_1_6.main();
-
-        Substitution_1_7 sub_1_7 = new Substitution_1_7();
-        sub_1_7.main();
-
-        Substitution_1_8 sub_1_8 = new Substitution_1_8();
-        sub_1_8.main();
-
-        Substitution_1_9 sub_1_9 = new Substitution_1_9();
-        sub_1_9.main();
-
-        Substitution_1_10 sub_1_10 = new Substitution_1_10();
-        sub_1_10.main();
-
-        Substitution_1_11 sub_1_11 = new Substitution_1_11();
-        sub_1_11.main();
-
-        Substitution_1_12 sub_1_12 = new Substitution_1_12();
-        sub_1_12.main();
+        CallSubstitution sub = new CallSubstitution();
+        sub.main();
     }
 }
