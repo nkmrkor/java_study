f1cee315bf4bee726be54648defb822ed738e95f
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-05-05 15:08:50 +0900

    練習問題 1 - 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         HelloWorld hw = new HelloWorld();
         hw.main();
+
         Substitution_1_2 sub_1_2 = new Substitution_1_2();
         sub_1_2.main();
+
+        Substitution_1_3 sub_1_3 = new Substitution_1_3();
+        sub_1_3.main();
     }
 }
---
+++ src/com/company/Substitution_1_3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by kaoru-nakamura2 on 2017/05/05.
+ */
+public class Substitution_1_3 {
+
+    public static void main() {
+        int x;
+        int y;
+
+        x = 13;
+        y = 17;
+
+        System.out.println( "x=" + x + ",y=" + y );
+    }
+}
