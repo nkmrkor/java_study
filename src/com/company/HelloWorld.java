f6cf54359a09a95d25f710570f4f88d33aacbf80
nkmrkor <kaoru-nakamura2@bm-sms.co.jp>
2017-05-05 11:15:35 +0900

    add HelloWorld.java

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by kaoru-nakamura2 on 2017/05/05.
+ */
+public class HelloWorld {
+    public static void main() {
+        System.out.println("Hello world!");
+    }
+}
